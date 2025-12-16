cb659624cc753dffed556dde16c0e7ee298718cd
dendy-one <danya.homon0202@gmail.com>
2025-12-16 10:45:17 +0100

    homeworkCalculator

---
+++ home_work3.1Calculator.py
@@ -0,0 +1,26 @@
+# Користувачеві водить почерзі два числа та дію над цими числами
+
+first_number = int(input("Enter first number: "))
+second_number = int(input("Enter second number: "))
+arithmetic_operation = input("Enter arithmetic operation: ")
+
+# Перевірку на те що при діленні дільник не дорівнює 0
+
+if arithmetic_operation == "/" and second_number == 0:
+    print("Error: division by zero")
+else:
+    match arithmetic_operation:
+        case "+":
+            print(first_number + second_number)
+
+        case "-":
+            print(first_number - second_number)
+
+        case "*":
+            print(first_number * second_number)
+
+        case "/":
+            print(first_number / second_number)
+
+        case _:
+            print("Invalid operation entered")
