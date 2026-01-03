11315aa2191ad4efa69f3d3447ada05df2643cb1
dendy-one <danya.homon0202@gmail.com>
2026-01-03 01:37:46 +0100

    homeworkUpdate

---
+++ home_work3.3lst.py
@@ -0,0 +1,20 @@
+first_list = [2, 4, 7, 11, 0, -2, 8]
+second_list = [1]
+third_list = []
+forth_list = [12, 3, 4, 10, 8]
+
+len_list = (len(first_list) + 1) // 2
+
+print(first_list[:len_list], first_list[len_list:])
+
+len_list = (len(second_list) + 1) // 2
+
+print(second_list[:len_list], second_list[len_list:])
+
+len_list = (len(third_list) + 1) // 2
+
+print(third_list[:len_list], third_list[len_list:])
+
+len_list = (len(forth_list) + 1) // 2
+
+print(forth_list[:len_list], forth_list[len_list:])
