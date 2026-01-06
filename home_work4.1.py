7fa8645a75cc530aef60f548e81ad8909c5240b3
dendy-one <danya.homon0202@gmail.com>
2026-01-06 22:37:08 +0100

    homework

---
+++ home_work4.1.py
@@ -0,0 +1,11 @@
+data = [
+    [0, 1, 0, 12, 3],
+    [0],
+    [1, 0, 13, 0, 0, 0, 5],
+    [9, 0, 7, 31, 0, 45, 0, 45, 0, 45, 0, 0, 96, 0],
+]
+
+for lane in data:
+    zeros_number = lane.count(0)
+    lane[:] = [num for num in lane if num != 0] + [0] * zeros_number
+    print(lane)
