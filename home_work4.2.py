98306f9c8fb2405e8bfaaad67176b77405a7fe7c
dendy-one <danya.homon0202@gmail.com>
2026-01-07 00:14:03 +0100

    homework

--- home_work4.2.py
+++ home_work4.2.py
@@ -5,7 +5,7 @@ date = [
     [0, 1, 7, 2, 4, 8],
 ]
 
-for line in date:
-    result = sum([line[num] for num in range(0, len(line), 2)])
-    last_element = line[len(line) - 1] if line != [] else 0
+for lane in date:
+    result = sum([lane[num] for num in range(0, len(lane), 2)])
+    last_element = lane[len(lane) - 1] if lane != [] else 0
     print(result * last_element)
