50c23477d61042a0800c4634f793767f96883d6a
dendy-one <danya.homon0202@gmail.com>
2026-01-07 00:13:29 +0100

    homework

--- home_work4.2.py
+++ home_work4.2.py
@@ -1,4 +1,9 @@
-date = [[1, 3, 5], [6], []]
+date = [
+    [1, 3, 5],
+    [6],
+    [],
+    [0, 1, 7, 2, 4, 8],
+]
 
 for line in date:
     result = sum([line[num] for num in range(0, len(line), 2)])
