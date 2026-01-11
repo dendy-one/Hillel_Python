2df4b6d9229e936d499992c254cbd36e93835a96
dendy-one <danya.homon0202@gmail.com>
2026-01-11 18:11:01 +0100

    homework

--- home_work6.1.py
+++ home_work6.1.py
@@ -3,7 +3,7 @@ import string
 span = input("Enter two letters separated by a hyphen: ")
 
 letters = string.ascii_letters
-start = letters.index(span[0])
-end = letters.index(span[2])
+start_index = letters.index(span[0])
+end_index = letters.index(span[2])
 
-print(letters[start : end + 1])
+print(letters[start_index : end_index + 1])
